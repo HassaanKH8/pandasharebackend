2d2b5edd8878b5b9347be1f41b821ec3a4f0c45e
Hassaan Ahmed <hassaankh8@gmail.com>
2024-12-17 18:33:46 +0500

    v1.9

--- server.js
+++ server.js
@@ -9,13 +9,12 @@ app.use(cors({ origin: '*', methods: ['GET', 'POST'], credentials: true }));
 const server = http.createServer(app);
 const io = new Server(server, {
     cors: { origin: '*', methods: ['GET', 'POST'], credentials: true },
-    maxHttpBufferSize: 10 * 1024 * 1024, // Set max buffer size to 10MB
+    maxHttpBufferSize: 10 * 1024 * 1024,
 });
 
 const fileSessions = {};
 const chunkedFiles = {};
 
-// Generate a unique session ID
 const generateSessionId = () => {
     return (Date.now().toString(36) + Math.random().toString(36).substr(2, 6)).toUpperCase();
 };
@@ -23,7 +22,6 @@ const generateSessionId = () => {
 io.on('connection', (socket) => {
     console.log('A user connected');
 
-    // Receive chunks
     socket.on('send-file-chunk', ({ sessionId, fileName, chunkIndex, totalChunks, chunkData }) => {
         if (!chunkedFiles[sessionId]) {
             chunkedFiles[sessionId] = {};
@@ -39,7 +37,6 @@ io.on('connection', (socket) => {
         const file = chunkedFiles[sessionId][fileName];
         file.chunks[chunkIndex] = Buffer.from(chunkData, 'base64');
 
-        // Check if all chunks have been received
         if (file.chunks.filter(Boolean).length === totalChunks) {
             const completeFile = Buffer.concat(file.chunks);
 
@@ -52,19 +49,19 @@ io.on('connection', (socket) => {
                 fileData: completeFile.toString('base64'),
             });
 
-            delete chunkedFiles[sessionId][fileName]; // Cleanup
+            delete chunkedFiles[sessionId][fileName];
             console.log(`Received complete file: ${fileName} for session ID: ${sessionId}`);
+
+            socket.emit('file-received', { sessionId, fileName });
         }
     });
 
-    // Notify client that session is ready
     socket.on('start-session', () => {
         const sessionId = generateSessionId();
         socket.emit('session-created', sessionId);
         console.log(`Session created: ${sessionId}`);
     });
 
-    // Fetch files
     socket.on('fetch-files', (sessionId) => {
         const sessionData = fileSessions[sessionId];
         if (sessionData && Date.now() - sessionData.createdAt < 3600000) {
